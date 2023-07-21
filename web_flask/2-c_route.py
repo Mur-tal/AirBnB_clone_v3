e33ce810aa8549aec069db48eb72f18b2aae1455
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-07-21 10:34:31 -0700

    Task #2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+    script that starts a Flask web application
+    listening in on 0.0.0.0 port 5000
+    route '/'
+          '/hbnb'
+          '/c/<text>'
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    '''display greeting'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' display "HBNB" '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    ''' display “C ” followed by the value of the text variable
+        and replace underscore _ symbols with a space
+    '''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
